fe76ab12d4e99aa2bcdbb94dfba2635f47a08124
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-21 12:03:06 +0000

    Updates

---
+++ precache-manifest.44edd4f5aa2b1a15b8725532659cf358.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ef5f29cc7d65c3aaba01dff92db0b1c",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "8c049c0e173b9d8a0f10",
+    "url": "/react-calculator/static/css/main.cd604b64.chunk.css"
+  },
+  {
+    "revision": "ba75aee36abcbcc9ff2a",
+    "url": "/react-calculator/static/js/2.099dd973.chunk.js"
+  },
+  {
+    "revision": "8c049c0e173b9d8a0f10",
+    "url": "/react-calculator/static/js/main.c5b6bb11.chunk.js"
+  },
+  {
+    "revision": "292def86a0fc950cdb37",
+    "url": "/react-calculator/static/js/runtime-main.933a4a8b.js"
+  }
+]);
