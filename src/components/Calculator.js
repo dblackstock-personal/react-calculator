11e617bc151d6b61d9d74ff69526a637386db0e3
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-22 17:12:09 +0000

    replaced callbacks with promises, it no longer works

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -21,56 +21,44 @@ export default class Calculator extends Component {
     }
 
     //this is called whenever we enter a number. It populates readout and either numberOne or numberTwo
-    buildSum = (input) => {
+    async buildSum(input) {
         if (this.state.operation !== "" && this.state.numberTwo === "") {   //this is for the case when we start inputting a second number
             // console.log("number outcome 1");
             // console.log("number 1 is "+this.state.numberOne);
-            this.setState(
-                {
-                    readout: input,
-                }, () => {
-                    this.setState({
-                        numberTwo: this.state.readout
-                    })
-                }
-            )
-
-        }else if(this.state.operation !== "" && this.state.numberOne === "") { //this is when we input the first number and we've just done a sum (so operator and second number have values)
-        // console.log("number outcome 2");
-        this.clear();
-        this.setState(
-            {
+            await new Promise (accept => this.setState({
                 readout: input,
-            }, () => {
-                this.setState({
-                    numberOne: this.state.readout
-                })
-            }
-        )
-        }else if(this.state.operation !== "") { //this is when we continue to input the second number
+            }, accept));
+            this.setState({
+                numberTwo: this.state.readout
+            })
+
+        } else if (this.state.operation !== "" && this.state.numberOne === "") { //this is when we input the first number and we've just done a sum (so operator and second number have values)
+            // console.log("number outcome 2");
+            this.clear();
+            await new Promise (accept => this.setState({
+                readout: input,
+            }, accept));
+            this.setState({
+                numberOne: this.state.readout
+            })
+
+        } else if (this.state.operation !== "") { //this is when we continue to input the second number
             // console.log("number outcome 3");
-            this.setState(
-                {
-                    readout: this.state.readout.toString() + input,
-                }, () => {
-                    this.setState({
-                        numberTwo: this.state.readout
-                    })
-                }
-            )
+            await new Promise(accept => this.setState({
+                readout: this.state.readout.toString() + input
+            }, accept));
+            this.setState({
+                numberTwo: this.state.readout
+            })
 
-        }else if (this.state.operation === "") {  //this is for when we are inputting the first number
-            // console.log("number outcome 4");
-            this.setState(
-                {
-                    readout: this.state.readout.toString() + input,
-                }, () => {
-                    this.setState({
-                        numberOne:this.state.readout
-                    })
-
-                }
-            )
+        } else if (this.state.operation === "") {  //this is for when we are inputting the first number
+            console.log("number outcome 4");
+            await new Promise(accept => this.setState({
+                readout: this.state.readout.toString() + input
+            }, accept));
+            this.setState({
+                numberOne: this.state.readout
+            })
         }
     }
 
@@ -79,7 +67,7 @@ export default class Calculator extends Component {
         this.setState({
             numberOne: this.state.readout,     //giving numberOne a value again since the equals cleared it
             numberTwo: "",                  //clearing numberTwo so that a new one can be set
-            operation:input                 //setting the new operation
+            operation: input                 //setting the new operation
         })
     }
 
@@ -90,13 +78,13 @@ export default class Calculator extends Component {
             // console.log("operation outcome 1");
             this.equals().then(this.setOperatorAfterEquals(input));
 
-        // if an operator is selected after equals (num2 and operator are populated), num1 should be populated from input
-        }else if(this.state.numberOne === "" && this.state.operation !== "" && this.state.readout !== "") {
+            // if an operator is selected after equals (num2 and operator are populated), num1 should be populated from input
+        } else if (this.state.numberOne === "" && this.state.operation !== "" && this.state.readout !== "") {
             // console.log("operation outcome 2");
             this.setOperatorAfterEquals(input);
 
-        // the else case covers inputs when numberOne is populated and numberTwo is empty
-        }else {
+            // the else case covers inputs when numberOne is populated and numberTwo is empty
+        } else {
             // console.log("operation outcome 3");
             this.setState(
                 {
@@ -109,57 +97,52 @@ export default class Calculator extends Component {
 
     //we need a function to handle each operator, so that it can be easilly called within the equals() promise
 
-    operation = (num1,num2) => {
+    operation = (num1, num2) => {
         let result;
-        num1=Number(num1);
-        num2=Number(num2);
+        num1 = Number(num1);
+        num2 = Number(num2);
         // console.log(this.state.operation);
-        switch(this.state.operation){
+        switch (this.state.operation) {
             case "+":
-            result = num1+num2;
-            break;
+                result = num1 + num2;
+                break;
             case "-":
-            result = num1-num2;
-            break;
+                result = num1 - num2;
+                break;
             case "/":
-            result = num1/num2;
-            break;
+                result = num1 / num2;
+                break;
             default:
-            result = "something has gone wrong";
+                result = "something has gone wrong";
         }
         // console.log(result);
         return result;
     }
 
-    equals = () => {
-        return new Promise((resolve,reject) => {
-            setTimeout( () => {
+     equals() {
+         console.log(this.state.numberTwo);
         // for the case where equals is run for the first time
-         if (this.state.numberOne !== "" && this.state.operation !== "" && this.state.readout !== "") { //this is suspect, why am I not looking at numberTwo?
+        if (this.state.numberOne !== "" && this.state.operation !== "" && this.state.readout !== "") { //this is suspect, why am I not looking at numberTwo?
             // console.log("equals outcome 1");
             // console.log(`Going into the equals, number1 is ${this.state.numberOne} with a type of ${typeof this.state.numberOne}`);
             // console.log(`Going into the equals, number2 is ${this.state.numberTwo} with a type of ${typeof this.state.numberTwo}`);
             // console.log(`Going into the equals, readout is ${this.state.readout} and operator is ${this.state.operation}`);
             let result = this.operation(this.state.numberOne, this.state.numberTwo);
             // console.log(`The result is ${result}`);
-            this.setState({
+             this.setState({
                 readout: result,
                 numberOne: ""
             })
             //this is if equals is pressed more than once
-         } else if (this.state.numberOne === "" && this.state.operation !== "" && this.state.readout !== "") {
+        } else if (this.state.numberOne === "" && this.state.operation !== "" && this.state.readout !== "") {
             // console.log("equals outcome 2");
             // console.log(`Going into the equals, number1 is ${this.state.numberOne}`);
             // console.log(`Going into the equals, number2 is ${this.state.numberTwo}`);
             let result = this.operation(this.state.readout, this.state.numberTwo);
-            this.setState({
+             this.setState({
                 readout: result
             })
-         }
-
-         resolve();
-        }, 100);
-        });
+        }
     }
 
     render() {
